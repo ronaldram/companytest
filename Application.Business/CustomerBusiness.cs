68f3cdd8d1dc810950c0efb5bc70b4bc9443dca1
Ronald Ameri Casas <ronald.ameri@tranzact.net>
2022-07-15 09:24:22 -0500

    my first commit

---
+++ Application.Business/CustomerBusiness.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Business
+{
+    public class CustomerBusiness
+    {
+        public IReadOnlyList<CustomerModel> GetAll()
+        {
+            var customers = new List<CustomerModel>
+            {
+                new CustomerModel(){Name="Jorge", Age = 10},
+                new CustomerModel(){Name="Jorge", Age = 10}
+            };
+            return customers;
+        }
+    }
+
+    public class CustomerModel
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+    }
+}
